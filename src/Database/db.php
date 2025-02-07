1f99882f639ed634534767327f51e70d8a4f8c74
ShamanK93 <158584781+ShamanK93@users.noreply.github.com>
2025-02-07 09:59:03 +0100

    Update db.php
    Ajout de $host $dbname $user $pass $options

--- src/Database/db.php
+++ src/Database/db.php
@@ -1,7 +1,14 @@
 <?php
-
+$host = 'localhost';
+$dbname = 'OwaspECommerce';
+$user = 'root';
+$pass = '';
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
 try {
-    $db = new mysqli("mysql:host=localhost;port=3306;dbname=OwaspECommerce", "root", "root");
+    $db = new mysqli("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $user, $pass, $options);
 } 
 catch (ErrorException $e) {
     echo $e;
